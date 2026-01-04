8d72c6df7603a17bd64cf8889912a15a2d10799a
Roshan Sanjeev <roshan.sanjeev@gmail.com>
2026-01-03 18:04:51 -1000

    Switch to Vercel adapter and enable server-side rendering for API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react(), tailwind()],
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
